95b688849f5acf636b8c8fc24842cbb09943039f
Aditya Kale <adityakalengp@gmail.com>
2020-02-06 19:09:55 +0530

    date

--- assignment_3.js
+++ assignment_3.js
@@ -1,5 +1,8 @@
-//Print 1 to 50 after after a gap of 1 second between two numbers.
-//Print 'Bye' at the end. Use node.js as a runtime engine for this.
+// //Print 1 to 50 after after a gap of 1 second between two numbers.
+// //Print 'Bye' at the end. Use node.js as a runtime engine for this.
+
+
+// with normal settimeout tick; 
 
 function addAsynchronously(a, callback) {
   setTimeout(function () {
@@ -18,4 +21,37 @@ while (count < 50) {
   // console.log("here")
   addAsynchronously(++count, printCount)
 }
+
+
+
+// ==============================
+// with time Date with objects
+// ==============================
+
+
+
+
+
+a = {};
+b = {};
+
+for (i = 0; i < 50; i++) {
+  a[i] = i;
+  b[i] = true;
+
+}
+count = 0;
+// console.log(JSON.stringify(a))
+
+var time = Date.now();
+
+while (count < 50) {
+  time2 = Math.floor((Date.now() - time) / 1000);
+  if (b[time2]) {
+    b[time2] = false
+    console.log(a[time2])
+    count++;
+  }
+}
+
 
